93bf5ed4f3a6f83004c2883ba44360c571b2447a
Sujeet Kumar Gupta <sujeetgupta2801@gmail.com>
2025-05-12 12:16:39 +0530

    chore: Set up Vite proxy for API requests to external server

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,14 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      "/api/v1/users": {
+        target: "https://ipoconnect-production.up.railway.app",
+        changeOrigin: true,
+        secure: false,
+      }
+    }
+  },
   plugins: [react()],
 })
