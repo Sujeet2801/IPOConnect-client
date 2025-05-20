baaecde65d71dea67dcf74acc57af5ba99b9ee38
Sujeet Kumar Gupta <sujeetgupta2801@gmail.com>
2025-05-20 22:26:57 +0530

    feat(client): Wrap main app routes with Layout and isolate auth routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,15 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import HeaderMain from "./components/header/HeaderMain";
-import HeaderLogin from "./components/header/HeaderLogin";
-import Home from "./components/home/Home";
-import BlogHome from "./page/blog/BlogHome";
-import Sector from "./page/sector/Sector";
-
-import ContactMain from "./components/contact/ContactMain";
 import FooterMain from "./components/footer/FooterMain";
+
+import Home from "./components/home/Home";
 
 import Upcoming_IPOs_Page from "./page/ipo/Upcoming_IPOs_Page";
 import Ongoing_IPOs_Page from "./page/ipo/Ongoing_IPOs_Page";
-import SectorDetailedPage from "./page/sector/SectorDetailedPage";
-import Shark_Detailed_Page from "./page/sharkInvestor/Shark_Detailed_Page";
+import Newlisted_IPO_Page from "./page/ipo/Newlisted_IPO_Page.jsx";
 
-import Mutual_Funds_Main from "./page/mutual_funds/Mutual_Funds_Main";
-
-import All_Brokers from "./page/broker/All_Brokers";
-import All_Shark_Investor from "./page/sharkInvestor/All_Shark_Investor";
+import BlogHome from "./page/blog/BlogHome";
 
 import TermsOfService from "./components/legal/TermsOfService";
 import PrivacyPolicy from "./components/legal/PrivacyPolicy";
@@ -26,39 +18,40 @@ import Disclaimer from "./components/legal/Disclaimers";
 import Compliance from "./components/legal/Compliance";
 import ScrollToTop from "./utility/scrollToTop.js";
 
+import { AuthProvider } from "./hooks/useAuth.jsx";
+import Layout from "./components/Layout/Layout.jsx";
+import { ToastContainer } from "react-toastify";
+
 function App() {
   return (
     <Router>
       <div className="bg-gray-100">
-
-        <ScrollToTop />
-        <HeaderMain />
-        <Routes>
+        
+        <ToastContainer />
+        <AuthProvider>
+          <ScrollToTop />
+          <HeaderMain />
+          <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/v1/api/users/" element={<Home />} />
-          <Route path="/v1/api/users/home" element={<Home />} />
+          <Route path="/home" element={<Home />} />
 
-          <Route path="/v1/api/users/login" element={<HeaderLogin />} />
-          <Route path="/v1/api/users/register" element={<HeaderLogin />} />
+          <Route path="/upcoming" element={<Upcoming_IPOs_Page />} />
+          <Route path="/Ongoing" element={<Ongoing_IPOs_Page />} />
+          <Route path="/Listed" element={<Newlisted_IPO_Page />} />
 
-          <Route path="/v1/api/users/sector" element={<Sector />} />
-          <Route path="/v1/api/users/blog" element={<BlogHome />} />
-          <Route path="/v1/api/users/shark-investor" element={<All_Shark_Investor />} />
-          <Route path="/v1/api/users/brokers" element={<All_Brokers />} />
+          <Route path="/blog" element={<BlogHome />} />
 
-          <Route path="/v1/api/users/upcoming" element={<Upcoming_IPOs_Page />} />
-          <Route path="/v1/api/users/Ongoing" element={<Ongoing_IPOs_Page />} />
-          <Route path="/v1/api/users/sector/:sectorName" element={<SectorDetailedPage />} />
-          <Route path="/v1/api/users/shark-investor/:sharkInvestor" element={<Shark_Detailed_Page />} />
-          <Route path="/v1/api/users/contact" element={<ContactMain />} />
+          <Route path="/termsOfservice" element={<TermsOfService />} />
+          <Route path="/privacypolicy" element={<PrivacyPolicy />} />
+          <Route path="/security" element={<Security />} />
+          <Route path="/disclaimer" element={<Disclaimer />} />
+          <Route path="/compliance" element={<Compliance />} />
 
-          <Route path="/v1/api/users/termsOfservice" element={<TermsOfService />} />
-          <Route path="/v1/api/users/privacypolicy" element={<PrivacyPolicy />} />
-          <Route path="/v1/api/users/security" element={<Security />} />
-          <Route path="/v1/api/users/disclaimer" element={<Disclaimer />} />
-          <Route path="/v1/api/users/compliance" element={<Compliance />} />
+          <Route path="/login" element={<Layout />} />
+          <Route path="/register" element={<Layout />} />
           
-        </Routes>
+          </Routes>
+        </AuthProvider>
         <FooterMain />
       </div>
     </Router>
