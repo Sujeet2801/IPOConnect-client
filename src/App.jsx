ab3aa08c3b8c706ed4ddb727fbe88eb383b5b5ee
Sujeet Kumar Gupta <sujeetgupta2801@gmail.com>
2025-05-26 01:28:29 +0530

    feat(client): Add routes for broker, sector listing and sector detail page

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,22 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import HeaderMain from "./components/header/HeaderMain";
-import FooterMain from "./components/footer/FooterMain";
-
 import Home from "./components/home/Home";
+import BlogHome from "./page/blog/BlogHome";
+import Sector from "./page/sector/Sector";
+
+import ContactMain from "./components/contact/ContactMain";
+import FooterMain from "./components/footer/FooterMain";
 
 import Upcoming_IPOs_Page from "./page/ipo/Upcoming_IPOs_Page";
 import Ongoing_IPOs_Page from "./page/ipo/Ongoing_IPOs_Page";
 import Newlisted_IPO_Page from "./page/ipo/Newlisted_IPO_Page.jsx";
 
-import BlogHome from "./page/blog/BlogHome";
+import SectorDetailedPage from "./page/sector/SectorDetailedPage";
+import Shark_Detailed_Page from "./page/sharkInvestor/Shark_Detailed_Page";
+import All_Shark_Investor from "./page/sharkInvestor/All_Shark_Investor";
+
+import All_Brokers from "./page/broker/All_Brokers";
 
 import TermsOfService from "./components/legal/TermsOfService";
 import PrivacyPolicy from "./components/legal/PrivacyPolicy";
@@ -35,11 +42,21 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/home" element={<Home />} />
 
+          <Route path="/blog" element={<BlogHome />} />
+
           <Route path="/upcoming" element={<Upcoming_IPOs_Page />} />
           <Route path="/Ongoing" element={<Ongoing_IPOs_Page />} />
           <Route path="/Listed" element={<Newlisted_IPO_Page />} />
 
-          <Route path="/blog" element={<BlogHome />} />
+          <Route path="/sector" element={<Sector />} />
+          <Route path="/sector/:sectorName" element={<SectorDetailedPage />} />
+
+          <Route path="/shark-investor" element={<All_Shark_Investor />} />
+          <Route path="/shark-investor/:sharkName" element={<Shark_Detailed_Page />} />
+
+          <Route path="/brokers" element={<All_Brokers />} />
+
+          <Route path="/contact" element={<ContactMain />} />
 
           <Route path="/termsOfservice" element={<TermsOfService />} />
           <Route path="/privacypolicy" element={<PrivacyPolicy />} />
