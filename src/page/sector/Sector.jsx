4a742e474238aabefce74b20e7ed51ba49a45cb2
Sujeet Kumar Gupta <sujeetgupta2801@gmail.com>
2025-05-26 01:23:40 +0530

    feat(client/page/sector): Add Sector component with auth-based conditional rendering

--- src/page/sector/Sector.jsx
+++ src/page/sector/Sector.jsx
@@ -1,14 +1,21 @@
-import React from 'react'
-import SectorSearch from './SectorSearch'
-import SectorData from './SectorData'
+import React from 'react';
+import SectorData from './SectorData';
+import { useAuth } from '../../hooks/useAuth';
 
 function Sector() {
+  const { user } = useAuth();
+
   return (
     <div>
-        {/* <SectorSearch/> */}
-        <SectorData/>
+      {user ? (
+        <SectorData />
+      ) : (
+        <p className="text-center text-gray-500 mt-10">
+          Please log in to view sector information.
+        </p>
+      )}
     </div>
-  )
+  );
 }
 
-export default Sector
+export default Sector;
