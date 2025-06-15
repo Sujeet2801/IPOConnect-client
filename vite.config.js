5015d7f2c9ac67a92f85a5f707b55de9c82fd260
Sujeet Kumar Gupta <sujeetgupta2801@gmail.com>
2025-06-15 12:50:52 +0530

    chore(client): Update backend Url

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vite.dev/config/====
 export default defineConfig({
   server: {
     proxy: {
       "/api/v1/users": {
-        target: "https://ipoconnect-production.up.railway.app",
+        target: "https://ipoconnect-website.onrender.com/",
         changeOrigin: true,
         secure: false,
       }
